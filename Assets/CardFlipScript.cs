cbac9e74c083de828dc20ca5144bd1b1382c91fc
sandra-assaf <sandra.alassaf@gmail.com>
2022-07-20 00:23:30 +0300

    tarot card flip

---
+++ Assets/CardFlipScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardFlipScript : MonoBehaviour
+{
+    public GameObject cardFront;
+    public GameObject cardBack;
+
+    public float rotationSpeed = 2f;
+    private float animationSum = 0;
+    private bool animateCard = false;
+
+
+    private void FixedUpdate()
+    {
+        if(this.animationSum >= 90)
+        {
+            this.animationSum = 0;
+            if (cardFront.active)
+            {
+                this.animateCard = false;
+
+            } else
+            {
+                cardFront.SetActive(true);
+                cardBack.SetActive(false);
+            }
+        }
+
+        if (this.animateCard)
+        {
+            GameObject activeSide = cardFront.active ? cardFront : cardBack;
+            int rotationDirection = cardFront.active ? 1 : -1;
+            activeSide.gameObject.transform.Rotate(0, rotationDirection * rotationSpeed, 0);
+            this.animationSum += rotationSpeed;
+        }   
+    }
+
+    public void OnMouseDown()
+    {
+        this.animateCard = true;
+        Debug.Log("clicked");
+    }
+}
